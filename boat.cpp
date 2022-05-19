e44140b37601156f801ea76101d7867363213d9f
Haritha Kannangara <99599781+it21308734@users.noreply.github.com>
2022-05-19 14:57:56 +0000

    IT21308734

---
+++ boat.cpp
@@ -0,0 +1,14 @@
+//IT21308734 | Kannangara K.K.H.R
+
+#include <cstring>
+#include "boat.h"
+
+Boat::setBoatDetails(int boId, string boType)
+{
+    boatId = boId;
+    boatType = boType; 
+}
+
+void Boat::addBoat() {}
+
+void Boat::rentBoat() {}
