ba092fa7984cd0a861f89277e94277213072297a
Pallage P.D.R.S <99821189+IT21311604@users.noreply.github.com>
2022-05-19 15:00:26 +0000

    IT21311604

---
+++ booking.cpp
@@ -0,0 +1,22 @@
+//IT21311604 | Pallage P.D.R.S
+
+#include <cstring>
+#include "booking.h"
+
+Booking::setBooking(int bDate, string bStatus)
+{
+    date = bDate;
+    status = bStatus;
+}
+
+void Booking::calcSubTotal() {}
+
+void Booking::calcTax() {}
+
+void Booking::calcTotal() {}
+
+Booking::setBookingDetails(int bdBookingId, string bdPaymentStatus)
+{
+    bookingId = bdBookingId;
+    paymentStatus = bdPaymentStatus;
+} 
