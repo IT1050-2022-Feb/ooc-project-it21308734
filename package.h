22f37fed069ccb75f2bca37d49c4509b053da7d9
it21310164 <99638391+it21310164@users.noreply.github.com>
2022-05-19 15:08:40 +0000

    it21310164

---
+++ package.h
@@ -0,0 +1,16 @@
+//IT21310164 | Subhath H.R.H
+
+#include <cstring>
+
+class Package
+{
+private:
+    int packageId;
+    string packageType;
+
+
+public:
+    void setPackageDetails(int pacId, string pacType);
+    void displaypackages();
+    void addItemsToPackages();
+}; 
